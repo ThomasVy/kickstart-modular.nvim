e6e4f0bf9519377791d1b6db9e894998b85baae0
Thomas Vy <vythomas97@gmail.com>
2024-07-31 21:43:38 -0600

    Adds more of my favourite things in vim

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -208,6 +208,10 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'clangd',
+        'typescript-language-server',
+        'clang-format',
+        'tailwindcss-language-server',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
