85f19287bf43410949da59fc7f5704b3c6b0d66e
Thomas Vy <vythomas97@gmail.com>
2024-08-14 19:03:39 -0600

    Harpoon changes

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -6,18 +6,39 @@ return {
         local harpoon = require 'harpoon'
         harpoon:setup()
 
-        vim.keymap.set('n', '<leader>na', function()
+        vim.keymap.set('n', '<leader>ny', function()
             harpoon:list():add()
-        end, { desc = 'Harpoo[N] [A]dd' })
+        end, { desc = 'Harpoo[N] Add' })
         vim.keymap.set('n', '<leader>nl', function()
             harpoon.ui:toggle_quick_menu(harpoon:list())
         end, { desc = 'Harpoo[N] [L]ist' })
 
-        -- Set <space>1..<space>5 be my shortcuts to moving to the files
-        for _, idx in ipairs { 1, 2, 3, 4, 5 } do
-            vim.keymap.set('n', string.format('<space>n%d', idx), function()
-                harpoon:list():select(idx)
-            end, { desc = string.format('Harpoo[N] [%d]', idx) })
-        end
+        -- select
+        vim.keymap.set('n', '<leader>na', function()
+            harpoon:list():select(1)
+        end, { desc = 'Harpoon 1' })
+        vim.keymap.set('n', '<leader>ns', function()
+            harpoon:list():select(2)
+        end, { desc = 'Harpoon 2' })
+        vim.keymap.set('n', '<leader>nd', function()
+            harpoon:list():select(3)
+        end, { desc = 'Harpoon 3' })
+        vim.keymap.set('n', '<leader>nf', function()
+            harpoon:list():select(4)
+        end, { desc = 'Harpoon 4' })
+
+        --replace
+        vim.keymap.set('n', '<leader>nra', function()
+            harpoon:list():select(1)
+        end, { desc = 'Replace Harpoon 1' })
+        vim.keymap.set('n', '<leader>nrs', function()
+            harpoon:list():select(2)
+        end, { desc = 'Replace Harpoon 2' })
+        vim.keymap.set('n', '<leader>nrd', function()
+            harpoon:list():select(3)
+        end, { desc = 'Replace Harpoon 3' })
+        vim.keymap.set('n', '<leader>nrf', function()
+            harpoon:list():select(4)
+        end, { desc = 'Replace Harpoon 4' })
     end,
 }
