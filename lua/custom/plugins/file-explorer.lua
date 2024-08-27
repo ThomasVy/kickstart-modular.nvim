16dd6cf4fe1318570bbc5040a53e16fd050ff1cc
Thomas Vy <vythomas97@gmail.com>
2024-08-26 22:59:06 -0600

    Removes oil and uses neotree instead

--- lua/custom/plugins/file-explorer.lua
+++ lua/custom/plugins/file-explorer.lua
@@ -27,54 +27,54 @@ return {
                     },
                 },
             }
-            vim.keymap.set('n', '<leader>tt', '<Cmd>Neotree toggle<CR>', { desc = 'Neotree toggle' })
+            vim.keymap.set('n', '-', '<Cmd>Neotree toggle<CR>', { desc = 'Neotree toggle' })
         end,
     },
-    {
-        'stevearc/oil.nvim',
-        opts = {},
-        -- Optional dependencies
-        -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-        dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
-
-        config = function()
-            local oil = require 'oil'
-            oil.setup {
-                default_file_explorer = false,
-                prompt_save_on_select_new_entry = true,
-                delete_to_trash = false,
-                skip_confirm_for_simple_edits = true,
-                lsp_file_methods = {
-                    -- Time to wait for LSP file operations to complete before skipping
-                    timeout_ms = 1000,
-                    -- Set to true to autosave buffers that are updated with LSP willRenameFiles
-                    -- Set to "unmodified" to only save unmodified buffers
-                    autosave_changes = true,
-                },
-                view_options = {
-                    -- Show files and directories that start with "."
-                    show_hidden = true,
-                    -- This function defines what is considered a "hidden" file
-                    is_hidden_file = function(name, bufnr)
-                        return vim.startswith(name, '.')
-                    end,
-                    -- This function defines what will never be shown, even when `show_hidden` is set
-                    is_always_hidden = function(name, bufnr)
-                        return false
-                    end,
-                    -- Sort file names in a more intuitive order for humans. Is less performant,
-                    -- so you may want to set to false if you work with large directories.
-                    natural_order = true,
-                    -- Sort file and directory names case insensitive
-                    case_insensitive = false,
-                    sort = {
-                        -- sort order can be "asc" or "desc"
-                        -- see :help oil-columns to see which columns are sortable
-                        { 'type', 'asc' },
-                        { 'name', 'asc' },
-                    },
-                },
-            }
-        end,
-    },
+    -- {
+    --     'stevearc/oil.nvim',
+    --     opts = {},
+    --     -- Optional dependencies
+    --     -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    --     dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
+    --
+    --     config = function()
+    --         local oil = require 'oil'
+    --         oil.setup {
+    --             default_file_explorer = false,
+    --             prompt_save_on_select_new_entry = true,
+    --             delete_to_trash = false,
+    --             skip_confirm_for_simple_edits = true,
+    --             lsp_file_methods = {
+    --                 -- Time to wait for LSP file operations to complete before skipping
+    --                 timeout_ms = 1000,
+    --                 -- Set to true to autosave buffers that are updated with LSP willRenameFiles
+    --                 -- Set to "unmodified" to only save unmodified buffers
+    --                 autosave_changes = true,
+    --             },
+    --             view_options = {
+    --                 -- Show files and directories that start with "."
+    --                 show_hidden = true,
+    --                 -- This function defines what is considered a "hidden" file
+    --                 is_hidden_file = function(name, bufnr)
+    --                     return vim.startswith(name, '.')
+    --                 end,
+    --                 -- This function defines what will never be shown, even when `show_hidden` is set
+    --                 is_always_hidden = function(name, bufnr)
+    --                     return false
+    --                 end,
+    --                 -- Sort file names in a more intuitive order for humans. Is less performant,
+    --                 -- so you may want to set to false if you work with large directories.
+    --                 natural_order = true,
+    --                 -- Sort file and directory names case insensitive
+    --                 case_insensitive = false,
+    --                 sort = {
+    --                     -- sort order can be "asc" or "desc"
+    --                     -- see :help oil-columns to see which columns are sortable
+    --                     { 'type', 'asc' },
+    --                     { 'name', 'asc' },
+    --                 },
+    --             },
+    --         }
+    --     end,
+    -- },
 }
