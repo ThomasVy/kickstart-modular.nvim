67ac9a0cf386cac87f01ecaa008367fd03671b37
Thomas Vy <vythomas97@gmail.com>
2024-09-05 12:44:05 -0600

    Can modify splits

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -48,6 +48,11 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- These mappings control the size of splits (height/width)
+vim.keymap.set('n', '<M-,>', '<c-w>5<')
+vim.keymap.set('n', '<M-.>', '<c-w>5>')
+vim.keymap.set('n', '<M-t>', '<C-W>+')
+vim.keymap.set('n', '<M-s>', '<C-W>-')
 
 vim.keymap.set('c', '<cr>', function()
     if vim.fn.pumvisible() == 1 then
