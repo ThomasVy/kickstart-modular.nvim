b586b65f1e83f47e1a58a32ff2b8ae97402e594b
Thomas Vy <vythomas97@gmail.com>
2024-11-17 18:49:24 +0000

    Do not force modules to be installed

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -206,15 +206,15 @@ return {
             -- for you, so that they are available from within Neovim.
             local ensure_installed = vim.tbl_keys(servers or {})
             vim.list_extend(ensure_installed, {
-                'stylua', -- Used to format Lua code
-                'emmet-language-server',
-                'clangd',
-                'tailwindcss-language-server',
-                'isort',
-                'black',
-                'prettier',
-                'npm-groovy-lint',
-                'cmakelang',
+                -- 'stylua', -- Used to format Lua code
+                -- 'emmet-language-server',
+                -- 'clangd',
+                -- 'tailwindcss-language-server',
+                -- 'isort',
+                -- 'black',
+                -- 'prettier',
+                -- 'npm-groovy-lint',
+                -- 'cmakelang',
             })
             require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
