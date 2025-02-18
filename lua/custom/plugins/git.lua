072113404b5fe4a2da2efa0ffc1bcb0452c5ce2d
Thomas Vy <vythomas97@gmail.com>
2025-02-17 23:42:06 -0700

    Remove copilot

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -1,19 +1,4 @@
 return {
-
-   {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "main",
-    dependencies = {
-      { "github/copilot.vim" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-    },
-    opts = {
-      debug = true, -- Enable debugging
-      -- See Configuration section for rest
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
-  {
     {
       'tpope/vim-fugitive',
       config = function()
@@ -53,5 +38,4 @@ return {
         vim.keymap.set('n', '<leader>gp', '<cmd>diffput //1<CR>', { desc = 'Put both into 1' })
       end,
     },
-  },
 }
