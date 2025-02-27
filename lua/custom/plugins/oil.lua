424ba18de79507f83aa5d1cf203a944cf23d0759
Thomas Vy <vythomas97@gmail.com>
2025-02-27 12:43:43 -0700

    fix(oil): show hidden files

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -8,6 +8,7 @@ return {
         config = function()
             require('oil').setup {
                 default_file_explorer = true,
+
                 delete_to_trash = false,
                 skip_confirm_for_simple_edits = true,
                 keymaps = {
@@ -27,6 +28,18 @@ return {
                     ['gx'] = 'actions.open_external',
                     ['g.'] = 'actions.toggle_hidden',
                     ['g\\'] = 'actions.toggle_trash',
+                },
+                view_options = {
+                    -- Show files and directories that start with "."
+                    show_hidden = true,
+                    is_always_hidden = function(name, bufnr)
+                        local hiddenFolders = { '.git', 'node_modules', '.vscode', '.vs' }
+                        for _, folder in ipairs(hiddenFolders) do
+                            if name == folder then
+                                return true
+                            end
+                        end
+                    end,
                 },
             }
             vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
