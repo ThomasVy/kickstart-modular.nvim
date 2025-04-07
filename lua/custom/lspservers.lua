649ea1f4c06491c669f239d870287ce256ff3950
Thomas Vy <vythomas97@gmail.com>
2025-04-07 11:24:13 -0600

    Adds keys properly to the clangd
    You can only use settings in servers

--- lua/custom/lspservers.lua
+++ lua/custom/lspservers.lua
@@ -18,6 +18,11 @@ require('custom.custom_lsps.rust')
 local servers = {
     clangd = {
         filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
+        settings = {
+            on_attach = {
+                vim.keymap.set('n', '<leader>o', '<Cmd>ClangdSwitchSourceHeader<CR>', { silent = true }),
+            },
+        },
         cmd = {
             "clangd",
             "--background-index",
@@ -25,29 +30,8 @@ local servers = {
             "--header-insertion=iwyu",
             "--completion-style=detailed",
             "--function-arg-placeholders",
-            "--fallback-style=llvm",
-        },
-        root_dir = function(fname)
-            return require("lspconfig.util").root_pattern(
-                "Makefile",
-                "configure.ac",
-                "configure.in",
-                "config.h.in",
-                "meson.build",
-                "meson_options.txt",
-                "build.ninja"
-            )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
-                fname
-            ) or require("lspconfig.util").find_git_ancestor(fname)
-        end,
-        keys = {
-            { '<leader>o', "<Cmd>ClangdSwitchSourceHeader<CR>", desc = "Switch Source/Header (C/C++)" },
-        },
-        init_options = {
-            usePlaceholders = true,
-            completeUnimported = true,
-            clangdFileStatus = true,
-        },
+            "--fallback-style=llvm"
+        }
     },
     dockerls = {},
     docker_compose_language_service = {},
