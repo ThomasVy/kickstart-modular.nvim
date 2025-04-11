b140fda53a262ac9513bc03b5f378e498fd80227
Thomas Vy <vythomas97@gmail.com>
2025-04-10 22:23:44 -0600

    updates to 1.*"

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -1,7 +1,7 @@
 return {
     {
         'saghen/blink.cmp',
-        version = '*',
+        version = '1.*',
         -- !Important! Make sure you're using the latest release of LuaSnip
         -- `main` does not work at the moment
         dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
