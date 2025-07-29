76cfeb92a737bd977053b317369994abb14b073b
Thomas Vy <vythomas97@gmail.com>
2025-07-29 00:03:29 -0600

    adds neotest back

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -1,37 +1,58 @@
 return {
-  -- "nvim-neotest/neotest",
-  -- dependencies = {
-  --   "nvim-neotest/nvim-nio",
-  --   "nvim-lua/plenary.nvim",
-  --   "antoinemadec/FixCursorHold.nvim",
-  --   "nvim-treesitter/nvim-treesitter",
-  --
-  --
-  --   "nvim-neotest/neotest-vim-test",
-  --   "vim-test/vim-test"
-  -- },
-  -- config = function()
-  --   require("neotest").setup({
-  --     adapters = {
-  --       require("neotest-vim-test")({ allow_file_types = { "haskell", "elixir" } }),
-  --     }
-  --   })
-  -- end,
-  'vim-test/vim-test',
+  "nvim-neotest/neotest",
   dependencies = {
-    'tpope/vim-dispatch', -- Recommended for async execution
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+
+
+    "nvim-neotest/neotest-vim-test",
+    "alfaix/neotest-gtest"
   },
   config = function()
-    -- vim-test configuration (example)
-    vim.g['test#framework'] = 'google_test' -- Or 'google_test', 'ctest', 'pytest', etc.
-    vim.g['test#neovim_only'] = 1
-    vim.g['test#cpp#google_test#make_command'] = 'cl'
-
-    -- Keybindings (example)
-    vim.api.nvim_set_keymap('n', '<leader>tn', ':TestNearest<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>ts', ':TestSuite<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>tf', ':TestFile<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>tl', ':TestLast<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>ts', ':TestVisit<CR>', { noremap = true, silent = true })
+    require("neotest").setup({
+      adapters = {
+        require("neotest-gtest").setup({
+        })
+      }
+    })
   end,
+  keys = {
+    {
+      "<leader>tn",
+      function()
+        require("neotest").run.run()
+      end,
+      desc = "Run Neotest",
+    },
+    {
+      "<leader>tf",
+      function()
+        require("neotest").run.run(vim.fn.expand("%"))
+      end,
+      desc = "Run Neotest on Current File",
+    },
+    {
+      "<leader>td",
+      function()
+        require("neotest").run.run({ strategy = "dap" })
+      end,
+      desc = "Run debugger on nearest test",
+    },
+    {
+      "<leader>ts",
+      function()
+        require("neotest").summary.toggle()
+      end,
+      desc = "Toggle Neotest Summary",
+    },
+    {
+      "<leader>to",
+      function()
+        require("neotest").output.open({ enter = true })
+      end,
+      desc = "Open Neotest Output",
+    },
+  }
 }
