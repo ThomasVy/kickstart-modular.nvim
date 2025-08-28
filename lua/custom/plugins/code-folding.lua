ac0df2b20279052dfe33dd5aad1da49d74a6a83d
Thomas Vy <vythomas97@gmail.com>
2025-08-28 11:03:29 -0600

    adds lsp as code folding struct

--- lua/custom/plugins/code-folding.lua
+++ lua/custom/plugins/code-folding.lua
@@ -2,11 +2,18 @@ return {
   "kevinhwang91/nvim-ufo",
   dependencies = { "kevinhwang91/promise-async" },
   event = "BufReadPost",
+  keys = {
+    { "zR", function() require("ufo").openAllFolds() end,               desc = "Open all folds" },
+    { "zM", function() require("ufo").closeAllFolds() end,              desc = "Close all folds" },
+    { "zr", function() require("ufo").openFoldsExceptKinds() end,       desc = "Open folds except kinds" },
+    { "zm", function() require("ufo").closeFoldsWith() end,             desc = "Close folds with" },
+    { "zK", function() require("ufo").peekFoldedLinesUnderCursor() end, desc = "Peek folded lines under cursor" },
+  },
   config = function()
     local ufo = require("ufo")
     ufo.setup({
       provider_selector = function(bufnr, filetype, buftype)
-        return { "treesitter", "indent" }
+        return { "lsp", "indent" }
       end,
     })
     vim.o.foldlevel = 99
