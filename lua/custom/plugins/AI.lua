bd8f372c85703f70e8348f994bfe21b499f9386d
Thomas Vy <vythomas97@gmail.com>
2025-09-16 14:46:20 -0600

    remove the auto_suggestion enable

--- lua/custom/plugins/AI.lua
+++ lua/custom/plugins/AI.lua
@@ -32,9 +32,9 @@ return {
       instructions_file = "avante.md",
       -- for example
       provider = "copilot",
-      mode = "legacy",           -- "legacy" | "agentic"
+      mode = "legacy",            -- "legacy" | "agentic"
       behaviour = {
-        auto_suggestions = true, -- Experimental stage
+        auto_suggestions = false, -- Experimental stage
         auto_set_highlight_group = true,
         auto_set_keymaps = true,
         auto_apply_diff_after_generation = true,
