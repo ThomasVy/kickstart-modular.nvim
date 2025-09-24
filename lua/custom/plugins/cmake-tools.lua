4538a7c193f29bdb05ac5645c3c6304458abc7ce
Thomas Vy <vythomas97@gmail.com>
2025-09-24 00:36:21 -0600

    adds cmake run to cmake tools shortcut

--- lua/custom/plugins/cmake-tools.lua
+++ lua/custom/plugins/cmake-tools.lua
@@ -2,10 +2,10 @@ return {
   'Civitasv/cmake-tools.nvim',
   opts = {
     cmake_generate_on_save = false,
-
   },
   keys = { -- load the plugin only when using it's keybinding:
     { '<leader>cg', ":CMakeGenerate<cr>", 'CMake generate' },
     { '<leader>cb', ":CMakeBuild<cr>",    'Cmake build' },
+    { '<leader>cr', ":CMakeRun<cr>",      'Cmake run' },
   },
 }
