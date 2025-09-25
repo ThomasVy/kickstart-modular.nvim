a082ea92a91d8708d97d067d5af5d567d1ed64c3
Thomas Vy <vythomas97@gmail.com>
2025-09-25 09:28:18 -0600

    overseer

--- lua/custom/plugins/overseer.lua
+++ lua/custom/plugins/overseer.lua
@@ -1,6 +1,12 @@
 return {
   'stevearc/overseer.nvim',
   opts = {},
+  keys = {
+    { "<leader>ot", "<cmd>OverseerToggle<cr>",      desc = "Toggle Overseer" },
+    { "<leader>or", "<cmd>OverseerRun<cr>",         desc = "Run Task" },
+    { "<leader>ob", "<cmd>OverseerBuild<cr>",       desc = "Build Task" },
+    { "<leader>oq", "<cmd>OverseerQuickAction<cr>", desc = "Quick Action" }
+  },
   config = function()
     require("overseer").setup({
       templates = { "builtin", "user.cpp_build" },
