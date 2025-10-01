3fcf5df4258e2ae93eb936510d06448ddf09e83e
Thomas Vy <vythomas97@gmail.com>
2025-10-01 13:32:36 -0600

    builds differently for cpp overseer in windows

--- lua/overseer/template/user/cpp_build.lua
+++ lua/overseer/template/user/cpp_build.lua
@@ -3,12 +3,22 @@ return {
     name = "g++ build",
     builder = function()
         -- Full path to current file (see :help expand())
+        -- if windows use cl.exe instead of g++
+
         local file = vim.fn.expand("%:p")
-        return {
-            cmd = { "g++" },
-            args = { file },
-            components = { { "on_output_quickfix", open = true }, "default" },
-        }
+        if vim.fn.has("win32") == 1 then
+            return {
+                cmd = { "cl.exe" },
+                args = { file },
+                components = { { "on_output_quickfix", open = true }, "default" },
+            }
+        else
+            return {
+                cmd = { "g++" },
+                args = { file },
+                components = { { "on_output_quickfix", open = true }, "default" },
+            }
+        end
     end,
     condition = {
         filetype = { "cpp" },
