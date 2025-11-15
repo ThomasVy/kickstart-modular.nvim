02543124822c0c7e7cd265c1f430a87b2925d1a3
Thomas Vy <tvy+sbg@cisco.com>
2025-11-14 18:26:08 -0700

    changes build and generate

--- lua/custom/plugins/cmake-tools.lua
+++ lua/custom/plugins/cmake-tools.lua
@@ -4,8 +4,8 @@ return {
     cmake_generate_on_save = false,
   },
   keys = { -- load the plugin only when using it's keybinding:
-    { '<leader>cg', ":CMakeGenerate<cr>", 'CMake generate' },
-    { '<leader>cb', ":CMakeBuild<cr>",    'Cmake build' },
+    { '<C-g>',      ":CMakeGenerate<cr>", 'CMake generate' },
+    { '<C-b>',      ":CMakeBuild<cr>",    'Cmake build' },
     { '<leader>cr', ":CMakeRun<cr>",      'Cmake run' },
   },
 }
