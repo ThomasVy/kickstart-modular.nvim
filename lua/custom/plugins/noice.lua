5eb6d1367f60ff6d11c72fa8ba9a697c57b47dab
Thomas Vy <vythomas97@gmail.com>
2026-01-09 00:19:11 -0700

    remove docs

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -15,6 +15,9 @@ return {
                     ['vim.lsp.util.stylize_markdown'] = false,
                     ['cmp.entry.get_documentation'] = false,
                 },
+                signature = {
+                    enabled = false,
+                },
             },
             presets = {
                 bottom_search = false,
@@ -24,16 +27,16 @@ return {
                 lsp_doc_border = true,
             },
             signature = {
-                enabled = true,
-                auto_open = {
-                    enabled = false,
-                    trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
-                    luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
-                    throttle = 50,  -- Debounce lsp signature help request by 50ms
-                },
-                view = nil,         -- when nil, use defaults from documentation
-                ---@type NoiceViewOptions
-                opts = {},          -- merged with defaults from documentation
+                enabled = false,
+                -- auto_open = {
+                --     enabled = false,
+                --     trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+                --     luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+                --     throttle = 50,  -- Debounce lsp signature help request by 50ms
+                -- },
+                -- view = nil,         -- when nil, use defaults from documentation
+                -- ---@type NoiceViewOptions
+                -- opts = {},          -- merged with defaults from documentation
             },
             routes = {
                 {
